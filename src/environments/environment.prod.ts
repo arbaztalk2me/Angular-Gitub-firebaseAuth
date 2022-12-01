b03bdd29492e28db3f6445e47612eaaa0c56435a
arbaztalk2me <arbaztalk2me@gmail.com>
2022-12-01 13:44:58 +0530

    added env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyBSFgA9YyGyt_fjbgX9ZvEsxmCuWdEyEWM",
+    authDomain: "githubpro-c79be.firebaseapp.com",
+    projectId: "githubpro-c79be",
+    storageBucket: "githubpro-c79be.appspot.com",
+    messagingSenderId: "691643324153",
+    appId: "1:691643324153:web:40aed5afa0d356b6c8b428"
+}
+}
